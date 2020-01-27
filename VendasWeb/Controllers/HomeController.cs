4e9f5913891dd69619b31c576c84d6026e0fe712
tatianadias16 <tatiana.dias1601@gmail.com>
2020-01-27 12:47:31 -0300

    Primeiro MVC

--- VendasWeb/Controllers/HomeController.cs
+++ VendasWeb/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using VendasWeb.Models;
+using VendasWeb.Models.ViewModels;
 
 namespace VendasWeb.Controllers
 {
--- VendasWeb/Models/ViewModels/ErrorViewModel.cs
+++ VendasWeb/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace VendasWeb.Models
+namespace VendasWeb.Models.ViewModels
 {
     public class ErrorViewModel
     {
