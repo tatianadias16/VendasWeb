1c86e21c1145e49073f7391bffde4f86b395f645
tatianadias16 <tatiana.dias1601@gmail.com>
2020-01-27 13:15:59 -0300

    CRUD Scaffolding

---
+++ VendasWeb/Data/VendasWebContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendasWeb.Models
+{
+    public class VendasWebContext : DbContext
+    {
+        public VendasWebContext (DbContextOptions<VendasWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendasWeb.Models.Department> Department { get; set; }
+    }
+}
